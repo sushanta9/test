23599556ba88fcf310f432994fc9a363555af0e4
root <root@mcjca032162.federated.fds>
2018-03-06 09:22:06 -0500

    changed setup.rb file to test ohai details

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -16,3 +16,14 @@ file '/opt/ckrcp/test1_file.txt' do
   group 'root'
 end
 
+file '/opt/ckrcp/hostdetail_file.txt' do 
+content "Please find the server details 
+   HOSTNAME: #{node['hostname']}
+   IPADDRESS: #{node['ipaddress']}
+   CPU: #{node['cpu']['0']['mhz']}
+   MEMORY: #{node['memory']['total']}"
+   owner 'root'
+   group 'root'
+   action 'create'
+end
+
